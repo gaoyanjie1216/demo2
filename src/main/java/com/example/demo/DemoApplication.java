7aac4a5018c9d90f73be1d5ec5a2f07fd204caf6
gao.yj <658583abc>
2018-04-09 22:11:43 +0800

    注释代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		//System.out.print(45345353);
+		//System.out.print(666);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
